b8d5e8f8a2b78ce46d5cc369609b370c7a8f10c3
bumsu <bumsu0211@gmail.com>
2019-07-18 01:30:46 +0900

    blog.models.Post 모델 생성/마이그레이션/admin등록

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100,
+    choices = (
+        ('제목1', '제목1 레이블'),
+        ('제목2', '제목2 레이블'),
+        ('제목3', '제목3 레이블'),
+    ))
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
