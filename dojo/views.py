d030229548af369574b14ff3984cc8def41ba3a7
bumsu <bumsu0211@gmail.com>
2019-07-17 17:13:09 +0900

    dojo.views.hello : 한글 문자열 매칭

--- dojo/views.py
+++ dojo/views.py
@@ -4,4 +4,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def mysum(request, numbers):
-    return HttpResponse(sum(map(int,numbers.split('/'))))
+    result = sum(map(lambda s: int(s or 0),numbers.split('/')))
+    return HttpResponse(result)
+
+def hello(request, name, age):
+    return HttpResponse('안녕하세요. {}. {}살이시네요.'.format(name,age))
