a6bc38659dc7c0535c8b8ba6c14048ce77751607
bumsu <bumsu0211@gmail.com>
2019-07-19 00:46:28 +0900

    django-debug-toolbar 셋업

--- askdjango/urls.py
+++ askdjango/urls.py
@@ -15,9 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
     path('dojo/', include('dojo.urls')),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        path('debug', include(debug_toolbar.urls))
+    ]
