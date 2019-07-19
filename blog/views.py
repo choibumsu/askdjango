eeecf63a02401099901c39e50eed3f45b9931245
bumsu <bumsu0211@gmail.com>
2019-07-19 11:50:28 +0900

    10강 마무리

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 # Create your views here.
@@ -10,4 +10,14 @@ def post_list(request):
     if q:
         qs = qs.filter(title__icontains=q)
 
-    return render(request, 'blog/post_list.html', {'post_list' : qs, 'q' : q})
+    return render(request, 'blog/post_list.html', {
+        'post_list' : qs,
+        'q' : q,
+    })
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+   
+    return render(request, 'blog/post_detail.html', {
+        'post' : post,
+    })
