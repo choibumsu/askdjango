9446b1aee796583a9563a86a5f07ff418cd597c9
bumsu <bumsu0211@gmail.com>
2019-07-25 16:06:12 +0900

    prac.models.Post 모델 validators 적용하고, model_form.save(commit=False) 활용 예시 추가

--- prac/forms.py
+++ prac/forms.py
@@ -1,11 +1,20 @@
 from django import forms
-
-def min_length_3_validator(value):
-    if len(value) < 3:
-        raise forms.ValidationError('3글자 이상 입력해주세요.')
+from .models import Post
 
 class PostForm(forms.Form):
+    class Meta:
+        model = Post
+        fields = ['title', 'content']
+    
+    '''
     title = forms.CharField(validators=[min_length_3_validator])
     content = forms.CharField(widget=forms.Textarea)
+    
+    def save(self, commit=True):
+        post = Post(**self.cleaned_data)
+        if commit:
+            post.save()
+        return post
+    '''
 
 
--- prac/views.py
+++ prac/views.py
@@ -7,7 +7,8 @@ def post_new(request):
     if request.method == 'POST':
         form = PostForm(request.POST, request.FILES) #인자 순서 바뀌면 안됨
         if form.is_valid():
-            post = Post.objects.create(**form.cleaned_data)
+            post = form.save(commit=False)
+            post.id = request.META['REMOTE_ADDR']
             post.save()
             return redirect('/prac/')
     else:
